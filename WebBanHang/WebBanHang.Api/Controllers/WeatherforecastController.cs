7751e0dfee536664f146b09aa2a8ced39631c345
dien2khust <nguyendien2804@gmail.com>
2023-04-24 05:46:42 +0700

    nvdien weatherforecast

---
+++ WebBanHang/WebBanHang.Api/Controllers/WeatherforecastController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBanHang.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class WeatherforecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "test";
+        }
+    }
+}
